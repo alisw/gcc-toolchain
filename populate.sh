61a1fb6ff726551e5176ff72f903b996cbbf5d22
Dario Berzano <dario.berzano@cern.ch>
2015-11-16 19:06:58 +0100

    GCC 4.9.3, Binutils 2.25.1, GDB 7.9

---
+++ populate.sh
@@ -0,0 +1,3 @@
+curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.25.1.tar.gz | tar xzf -
+curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.3/gcc-4.9.3.tar.gz | tar xzf -
+curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-7.9.tar.gz | tar xzf -
