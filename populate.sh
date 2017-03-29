566140dee4986314b24bc918d4760e7460f247c8
Dario Berzano <dario.berzano@cern.ch>
2017-03-29 17:05:12 +0200

    GCC 6.2.0, Binutils 2.27, GDB 7.12.1

--- populate.sh
+++ populate.sh
@@ -1,3 +1,5 @@
-curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.25.1.tar.gz | tar xzf -
-curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.3/gcc-4.9.3.tar.gz | tar xzf -
-curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-7.9.tar.gz | tar xzf -
+#!/bin/sh -ex
+rm -rf binutils* gcc* gdb*
+curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz | tar xzf -
+curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.gz | tar xzf -
+curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-7.12.1.tar.xz | tar xJf -
