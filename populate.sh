fb896572d4935e81cc322cec64ba7164cc4df22c
Dario Berzano <dario.berzano@cern.ch>
2018-03-01 11:33:03 +0000

    Import GCC 4.4.7, binutils 2.20.1a and GDB 7.2a
    RHEL 6 environment

---
+++ populate.sh
@@ -0,0 +1,3 @@
+curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.20.1a.tar.bz2 | tar xjf -
+curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-4.4.7/gcc-4.4.7.tar.gz | tar xzf -
+curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-7.2a.tar.bz2 | tar xjf -
