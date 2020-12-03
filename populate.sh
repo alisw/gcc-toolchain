38d386ec5458bc06cecc9032a8f787276f93e1b9
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-12-03 18:08:09 +0100

    Updated to GCC 9.3.0, binutils-2.35.1 gdb 10.1

--- populate.sh
+++ populate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ex
 rm -rf binutils* gcc* gdb*
-curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.xz | tar xJf -
-curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-7.3.0/gcc-7.3.0.tar.xz | tar xJf -
-curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.xz | tar xJf -
+curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.xz | tar xJf -
+curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz | tar xJf -
+curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz | tar xJf -
 ln -nfs binutils* binutils
 ln -nfs gcc* gcc
 ln -nfs gdb* gdb
