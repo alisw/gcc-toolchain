44b69af47db5370c3c097da910af39ea7000f45a
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-04-22 08:22:50 +0200

    Bump to GCC 10

--- gcc-9.3.0/gcc/testsuite/gfortran.dg/gomp/teams1.f90
+++
@@ -1,8 +0,0 @@
-! PR fortran/92756
-
-program pr92756
-  integer :: i
-  !$omp teams distribute parallel do	! { dg-error "'teams' construct not closely nested inside of 'target' construct" }
-  do i = 1, 64
-  end do
-end
--- populate.sh
+++ populate.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -ex
-rm -rf binutils* gcc* gdb* mpfr* isl* mpc* gmp*
-curl -l http://isl.gforge.inria.fr/isl-0.21.tar.gz | tar xzf -
+rm -rf binutils* gcc* gdb* mpfr* isl* mpc* gmp* autoconf-archive*
+curl -L http://isl.gforge.inria.fr/isl-0.21.tar.gz | tar xzf -
 curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.xz | tar xJf -
-curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz | tar xJf -
+curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz | tar xJf -
 curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz | tar xJf -
-curl -l https://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.xz | tar xJf -
-curl -l https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz | tar xzf -
-curl -l https://gmplib.org/download/gmp/gmp-6.2.0.tar.xz | tar xJf -
+curl -L https://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.xz | tar xJf -
+curl -L https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz | tar xzf -
+curl -L https://gmplib.org/download/gmp/gmp-6.2.0.tar.xz | tar xJf -
+curl -L https://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2021.02.19.tar.xz | tar xJf -
 
-ln -nfs binutils* binutils
-ln -nfs gcc* gcc
-ln -nfs gdb* gdb
-ln -fs ../mpfr gcc-*
-ln -fs ../isl gcc-*
-ln -fs ../mpc gcc-*
-ln -fs ../gmp gcc-*
-ln -nfs mpfr* mpfr
-ln -nfs isl* isl
-ln -nfs mpc* mpc
-ln -nfs gmp* gmp
+mv binutils* binutils
+mv gcc* gcc
+mv gdb* gdb
+mv autoconf-archive* autoconf-archive
+mv mpfr* gcc/mpfr
+mv gmp* gcc/gmp
+mv isl* gcc/isl
+mv mpc* gcc/mpc
+
+rm -fr gcc/.gitignore
