005fa181f275bf6d29fb3853f3d00ea3518529e2
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-11-22 09:56:52 +0100

    Bump the dependencies we have

--- populate.sh
+++ populate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ex
 rm -rf binutils* gcc* gdb* mpfr* isl* mpc* gmp* autoconf-archive*
-curl -L https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2 | tar -xjf -
-curl -L ftp://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.xz | tar xJf -
-curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz | tar xJf -
-curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-11.2.tar.xz | tar xJf -
+curl -L https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.25.tar.bz2 | tar -xjf -
+curl -L ftp://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz | tar xJf -
+curl -L ftp://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz | tar xJf -
+curl -L ftp://ftp.gnu.org/gnu/gdb/gdb-12.1.tar.xz | tar xJf -
 curl -L ftp://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz | tar xJf -
 curl -L ftp://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz | tar xzf -
 curl -L ftp://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz | tar xJf -
